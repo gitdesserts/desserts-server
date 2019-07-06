e962776acb660ea0d6a0f03014a64058c7fd65c6
Ahnteve <ahnteve@gmail.com>
2019-07-06 13:39:04 +0900

    Connect database

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,29 @@
+import * as dotenv from 'dotenv';
 import * as express from 'express';
+import { createConnection } from 'typeorm';
 
-const app = express();
+dotenv.config();
 
-app.get('/', (req, res) => {
-  res.send('express');
-});
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USERNAME, DB_PORT } = process.env;
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log('sever start');
+createConnection({
+  type: 'mysql',
+  username: DB_USERNAME,
+  password: DB_PASSWORD,
+  port: parseInt(DB_PORT),
+  host: DB_HOST,
+  database: DB_NAME,
+  synchronize: false,
+  entities: [],
+  dropSchema: false
+}).then(async () => {
+  const app: express.Application = express();
+
+  app.get('/', (req, res) => {
+    res.send('express');
+  });
+
+  app.listen(process.env.PORT || 3000, () => {
+    console.log(`Sever start http://localhost:${process.env.PORT || 3000}`);
+  });
 });
